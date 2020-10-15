26982b7b5d8e0336e599768607f8fcce722591d3
竹内 勇太 <16091@kauth.local>
2020-10-15 09:53:06 +0900

    Aがキャラ追加

---
+++ team6/huga.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class huga here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class huga extends Actor
+{
+    /**
+     * Act - do whatever the huga wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
