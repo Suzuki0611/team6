9332daaa10ce5977477994545977ccfaa20e779a
矢野 翔太郎 <16155@kauth.local>
2020-11-26 10:16:47 +0900

    箱を追加

--- team6/team6/hacchiman.java
+++ team6/team6/hacchiman.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class huga here.
+ * Write a description of class hacchiman here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class huga extends Actor
+public class hacchiman extends Actor
 {
     /**
-     * Act - do whatever the huga wants to do. This method is called whenever
+     * Act - do whatever the hacchiman wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
--- team6/team6/huga.java
+++ team6/team6/huga.java
@@ -14,6 +14,9 @@ public class huga extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) ){
+        setRotation(90);
+        move(1);
+}// Add your action code here.
     }    
 }
